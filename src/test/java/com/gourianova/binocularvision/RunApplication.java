9347bbb914bb4765794a9197fbab953544ed625c
Tatiana-Gourianova <62667085+Tatiana-Gourianova@users.noreply.github.com>
2021-07-25 20:09:23 +0300

    Add files via upload

---
+++ src/test/java/com/gourianova/binocularvision/RunApplication.java
@@ -0,0 +1,35 @@
+package com.gourianova.binocularvision;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class RunApplication {
+
+    private static UserRepository userRepository;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            UserRepository userRepository) {
+        return args -> {
+            List<User> userList = new ArrayList<>();
+            userList = userRepository.findAllByIdExists();
+            System.out.println(userList);
+        };
+
+    }
+
+}
+
+
+
